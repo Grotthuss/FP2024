d95034703da6b0294b89b3f84d624747d6b2c750
Matas <m.firantas@gmail.com>
2024-09-16 22:17:03 +0300

    repl

--- src/Main.hs
+++ src/Main.hs
@@ -24,65 +24,12 @@ read' = putStr "REPL> "
      >> hFlush stdout
      >> getLine
 
-data Command = AddBook String String Int
-             | ListBooks
-             | FindBook String String Int
-             | RemoveBook String String Int
-             deriving (Show, Eq)
 
-commandParser :: Parser Command
-commandParser = try addBookParser
-            <|> try listBooksParser
-            <|> try findBookParser
-            <|> try removeBookParser
-
-addBookParser :: Parser Command
-addBookParser = do
-    void $ string "add book"
-    spaces
-    title <- manyTill anyChar (char ',')
-    spaces
-    author <- manyTill anyChar (char ',')
-    spaces
-    year <- many digit
-    return $ AddBook title author (read year)
-
-listBooksParser :: Parser Command
-listBooksParser = ListBooks <$ string "list books"
-
-findBookParser :: Parser Command
-findBookParser = do
-    void $ string "find book"
-    spaces
-    title <- manyTill anyChar (char ',')
-    spaces
-    author <- manyTill anyChar (char ',')
-    spaces
-    year <- many digit
-    return $ FindBook title author (read year)
-
-removeBookParser :: Parser Command
-removeBookParser = do
-    void $ string "remove book"
-    spaces
-    title <- manyTill anyChar (char ',')
-    spaces
-    author <- manyTill anyChar (char ',')
-    spaces
-    year <- many digit
-    return $ RemoveBook title author (read year)
 
 
 
 eval' :: String -> String
-eval' input = case parse commandParser "" input of
-        Right cmd -> executeCommand cmd
-executeCommand :: Command -> String
-executeCommand (AddBook title author year) = "knyga prideta " ++ title ++ " autorius " ++ author ++ " (" ++ show year ++ ")"
-executeCommand ListBooks = "knygu dar nera"
-executeCommand (FindBook title author year) = "ieskoma knyga " ++ title ++ " autorius " ++ author ++ " (" ++ show year ++ ")"
-executeCommand (RemoveBook title author year) = "knyga istrinta " ++ title ++ " autorius " ++ author ++ " (" ++ show year ++ ")"
-
+eval' input = input
 
 
 print' :: String -> IO ()
